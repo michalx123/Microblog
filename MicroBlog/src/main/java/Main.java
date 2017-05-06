483c0b26d87fe369e3152f4c7e8b009e14106d31
Piotr <xenros@gmail.com>
2017-05-06 11:09:39 +0200

    dodawanie projektu michala

---
+++ MicroBlog/src/main/java/Main.java
@@ -0,0 +1,22 @@
+import static spark.Spark.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import spark.ModelAndView;
+import spark.TemplateEngine;
+import spark.template.freemarker.FreeMarkerEngine;
+
+public class Main {
+    public static void main(String[] args) {
+        get("/*", (request, response) -> {
+        	
+        	Map <String, Object> attributes = new HashMap<>();
+        	attributes.put("message","Hello World");
+        	int a = 100+10;
+        	attributes.put("dodawanie", a);
+        	return new ModelAndView(attributes, "hello.ftl");
+        }, new FreeMarkerEngine());
+        
+    }
+}
