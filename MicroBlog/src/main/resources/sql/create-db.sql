768779c5d8fab83f6d82f10e3e0415bddb49de5d
Michal Gluchowicz <m.gluchowicz@gmail.com>
2017-05-16 22:16:43 +0200

    sql Tabela + wpisy

--- MicroBlog/src/main/resources/sql/create-db.sql
+++ MicroBlog/src/main/resources/sql/create-db.sql
@@ -1,14 +1,17 @@
-drop table uzytkownik if exists; create table uzytkownik (
+drop table uzytkownik if exists; 
+create table uzytkownik (
 id_uzytkownika INT auto_increment primary key,
 imie varchar(45),
 nazwisko varchar(45),
 );
-drop table follower if exists; create table follower (
+drop table follower if exists; 
+create table follower (
 id_followera INT auto_increment primary key,
 imie varchar(45),
 nazwisko varchar(45),
 );
-drop table wpis if exists; create table wpis (
-id_2pisu INT auto_increment primary key,
-imie varchar(250),
+drop table wpis if exists;
+create table wpis (
+id_wpisu INT auto_increment primary key,
+tekst varchar(250),
 );
---
+++ MicroBlog/src/main/resources/sql/insert-data.sql
@@ -0,0 +1,6 @@
+INSERT INTO uzytkownik(imie,nazwisko) VALUES ('Jozef','Malinka');
+INSERT INTO uzytkownik(imie,nazwisko) VALUES ('Andrzej','Slodki'); 
+INSERT INTO follower(imie,nazwisko) VALUES ('Waldemar','Kiepski');
+INSERT INTO follower(imie,nazwisko) VALUES ('Marian','Pazdzioch'); 
+INSERT INTO wpis(tekst) VALUES ('Testowy wpis na tablicy');
+INSERT INTO wpis(tekst) VALUES ('Czesc i czolem!');
