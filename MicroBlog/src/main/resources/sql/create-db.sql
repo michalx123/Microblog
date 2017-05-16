9ed2575b557f816c4877d993575f31e96be30f5e
Michal Gluchowicz <m.gluchowicz@gmail.com>
2017-05-16 21:54:43 +0200

    sql Tabela

---
+++ MicroBlog/src/main/resources/sql/create-db.sql
@@ -0,0 +1,14 @@
+drop table uzytkownik if exists; create table uzytkownik (
+id_uzytkownika INT auto_increment primary key,
+imie varchar(45),
+nazwisko varchar(45),
+);
+drop table follower if exists; create table follower (
+id_followera INT auto_increment primary key,
+imie varchar(45),
+nazwisko varchar(45),
+);
+drop table wpis if exists; create table wpis (
+id_2pisu INT auto_increment primary key,
+imie varchar(250),
+);
