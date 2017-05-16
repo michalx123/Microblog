4b71ea0d25098aa980531f077b3ae35df234bb77
Michal Gluchowicz <m.gluchowicz@gmail.com>
2017-05-16 23:13:05 +0200

    HSQLDB

--- MicroBlog/src/main/resources/sql/create-db.sql
+++ MicroBlog/src/main/resources/sql/create-db.sql
@@ -1,17 +1,20 @@
 drop table uzytkownik if exists; 
 create table uzytkownik (
-id_uzytkownika INT auto_increment primary key,
+id_uzytkownika int GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1),
 imie varchar(45),
 nazwisko varchar(45),
+PRIMARY KEY (id_uzytkownika)
 );
 drop table follower if exists; 
 create table follower (
-id_followera INT auto_increment primary key,
+id_followera INT GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1),
 imie varchar(45),
 nazwisko varchar(45),
+PRIMARY KEY (id_followera)
 );
 drop table wpis if exists;
 create table wpis (
-id_wpisu INT auto_increment primary key,
+id_wpisu INT GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1),
 tekst varchar(250),
+PRIMARY KEY (id_wpisu)
 );
