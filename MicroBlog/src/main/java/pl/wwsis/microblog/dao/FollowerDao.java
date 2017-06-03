034080a0bfb89196b5258b34aa375ee4d896868b
Piotr Rosiński <Piotr Rosiski>
2017-06-03 11:10:16 +0200

    poprawka nazewnictwa metody DAO

--- MicroBlog/src/main/java/pl/wwsis/microblog/dao/FollowerDao.java
+++ MicroBlog/src/main/java/pl/wwsis/microblog/dao/FollowerDao.java
@@ -4,12 +4,9 @@ package pl.wwsis.microblog.dao;
 public interface FollowerDao {
 
 
-	public void addFollowee(int idUzytkownika,int idFollowera);
-	
+	public void addFollowee(int idUzytkownika,int idFollowera);	
 	public void deleteFollowee(int idUzytkownika,int idFollowera);
-	
-	
-	public void checkFollowee(int idUzytkownika);
+	public void checkFollowers(int idUzytkownika);
 	
 	
 
