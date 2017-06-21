af2663cff33d9fbc2aa50cda8b237ebd4df936c1
Piotr Rosiński <piotr.rosinski90@gmail.com>
2017-06-21 20:17:03 +0200

    addMessage

--- MicroBlog/src/main/java/com/wwsis/microblog/dao/impl/WpisDaoImpl.java
+++ MicroBlog/src/main/java/com/wwsis/microblog/dao/impl/WpisDaoImpl.java
@@ -42,7 +42,7 @@ public class WpisDaoImpl implements WpisDao{
 
 	@Override
 	public void addMessage(int idUzytkownika, String message) {
-		String hql  =  "INSERT INTO wpis (iduzytkownika,tekst)" + "SELECT iduzytkownika, message";
+		String hql  =  "INSERT INTO wpis (iduzytkownika,tekst)" + "SELECT :iduzytkownika, :message";
 		Query query  = entityManager . createQuery ( hql );
 		query.setParameter("iduzytkownika", idUzytkownika);
 		query.setParameter("message", message);
